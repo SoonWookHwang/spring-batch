b2cddea7e4999fc8a0aca95ade3f0b250ac793cf
SoonWookHwang <bravadosw@naver.com>
2023-05-15 14:45:31 +0900

    다양한 Step 설정

--- src/main/java/com/example/springbatch/job/config/ExampleJobConfig.java
+++ src/main/java/com/example/springbatch/job/config/ExampleJobConfig.java
@@ -1,11 +1,13 @@
 package com.example.springbatch.job.config;
 
+import java.sql.SQLException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -107,6 +109,50 @@ public class ExampleJobConfig {
           log.info("Write Step!");
           return RepeatStatus.FINISHED;
         })
+        .build();
+  }
+
+  @Bean
+  @JobScope
+  public Step Step1() throws Exception {
+    return stepBuilderFactory.get("Step")
+        .startLimit(3)
+        .<Member,Member>chunk(10)
+        .reader(reader(null))
+        .processor(processor(null))
+        .writer(writer(null))
+        .build();
+  }
+
+  @Bean
+  @JobScope
+  public Step Step2() throws Exception {
+    return stepBuilderFactory.get("Step")
+        .<Member,Member>chunk(10)
+        .reader(reader(null))
+        .processor(processor(null))
+        .writer(writer(null))
+        .faultTolerant()
+        .skipLimit(1) // skip 허용 횟수, 해당 횟수 초과시 Error 발생, Skip 사용시 필수 설정
+        .skip(NullPointerException.class)// NullPointerException에 대해선 Skip
+        .noSkip(SQLException.class) // SQLException에 대해선 noSkip
+        //.skipPolicy(new CustomSkipPolilcy) // 사용자가 커스텀하며 Skip Policy 설정 가능
+        .build();
+  }
+
+  @Bean
+  @JobScope
+  public Step Step3() throws Exception {
+    return stepBuilderFactory.get("Step")
+        .<Member,Member>chunk(10)
+        .reader(reader(null))
+        .processor(processor(null))
+        .writer(writer(null))
+        .faultTolerant()
+        .retryLimit(1) //retry 횟수, retry 사용시 필수 설정, 해당 Retry 이후 Exception시 Fail 처리
+        .retry(SQLException.class) // SQLException에 대해선 Retry 수행
+        .noRetry(NullPointerException.class) // NullPointerException에 no Retry
+        //.retryPolicy(new CustomRetryPolilcy) // 사용자가 커스텀하며 Retry Policy 설정 가능
         .build();
   }
 
