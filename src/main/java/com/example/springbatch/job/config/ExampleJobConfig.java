15ad21afae5a2b3e8a3ed6bf333dedd074f1819b
SoonWookHwang <bravadosw@naver.com>
2023-05-15 15:51:42 +0900

    JPA 기반의 Batch Job 구현하기

--- src/main/java/com/example/springbatch/job/config/ExampleJobConfig.java
+++ src/main/java/com/example/springbatch/job/config/ExampleJobConfig.java
@@ -2,7 +2,7 @@ package com.example.springbatch.job.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.sql.DataSource;
+import javax.persistence.EntityManagerFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -12,17 +12,13 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.PagingQueryProvider;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
-import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 /*
 전체 금액이 10000원 이상인 회원들에게 1000원 캐시백을 주는 배치
@@ -36,12 +32,12 @@ public class ExampleJobConfig {
   public JobBuilderFactory jobBuilderFactory;
   @Autowired
   public StepBuilderFactory stepBuilderFactory;
-
   @Autowired
-  public DataSource dataSource;
+  public EntityManagerFactory entityManagerFactory;
 
   @Bean
-  public Job ExampleJob() {
+  public Job ExampleJob() throws Exception {
+
     Job exampleJob = jobBuilderFactory.get("exampleJob")
         .start(Step())
         .build();
@@ -53,7 +49,7 @@ public class ExampleJobConfig {
   @JobScope
   public Step Step() throws Exception {
     return stepBuilderFactory.get("Step")
-        .<Member, Member>chunk(10)
+        .<Member, Memeber>chunk(10)
         .reader(reader())
         .processor(processor())
         .writer(writer())
@@ -62,20 +58,16 @@ public class ExampleJobConfig {
 
   @Bean
   @StepScope
-  public JdbcPagingItemReader<Member> reader() throws Exception {
-
+  public JpaPagingItemReader<Member> reader() throws Exception {
     Map<String, Object> parameterValues = new HashMap<>();
-    parameterValues.put("amount", "10000");
+    parameterValues.put("amount", 10000);
 
-    //pageSize와 fetchSize는 동일하게 설정
-
-    return new JdbcPagingItemReaderBuilder<Member>()
+    return new JpaPagingItemReaderBuilder<Member>()
         .pageSize(10)
-        .fetchSize(10)
-        .dataSource(new BeanPropertyRowMapper<>(Member.class))
-        .queryProvider(customQueryProvider())
         .parameterValues(parameterValues)
-        .name("JdbcPagingItemReader")
+        .queryString("SELECT p FROM Member p WHERE p.amount >= :amount ORDER BY id ASC")
+        .entityManagerFactory(entityManagerFactory)
+        .name("JpaPagingItemReader")
         .build();
   }
 
@@ -84,42 +76,21 @@ public class ExampleJobConfig {
   public ItemProcessor<Member, Member> processor() {
     return new ItemProcessor<Member, Member>() {
       @Override
-      public Member process(Member member) throws Exception {
+      public Member process(Member item) throws Exception {
 
-        //1000원 추가 적립
-        member.setAmount(member.getAmount() + 1000);
+        //1000원 추가적립
+        member.setAmount(memeber.getAmount() + 1000);
 
-        return member;
+        return memeber;
       }
     };
   }
 
-
   @Bean
   @StepScope
-  public JdbcBatchItemWriter<Member> writer() {
-    return new JdbcBatchItemWriterBuilder<Member>()
-        .dataSource(dataSource)
-        .sql("UPDATE MEMBER SET AMOUNT = :amount WHERE ID = :id")
-        .beanMapped()
+  public JpaItemWriter<Member> writer() {
+    return new JpaItemWriterBuilder<Member>()
+        .entityManagerFactory(entityManagerFactory)
         .build();
   }
-
-  public PagingQueryProvider customQueryProvider() throws Exception {
-    SqlPagingQueryProviderFactoryBean queryProviderFactoryBean = new SqlPagingQueryProviderFactoryBean();
-
-    queryProviderFactoryBean.setDataSource(dataSource);
-
-    queryProviderFactoryBean.setSelectClause("SELECT ID, NAME, EMAIL, NICK_NAME, STATUS, AMOUNT ");
-    queryProviderFactoryBean.setFromClause("FROM MEMBER ");
-    queryProviderFactoryBean.setWhereClause("WHERE AMOUNT >= :amount");
-
-    Map<String, Order> sortKey = new HashMap<>();
-    sortKey.put("id", Order.ASCENDING);
-
-    queryProviderFactoryBean.setSortKeys(sortKey);
-
-    return queryProviderFactoryBean.getObject();
-  }
-
 }
